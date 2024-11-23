88246f9daf11cecd725b758487b2f6614d0e8cb7
Aadiarora151 <107919173+Aadiarora151@users.noreply.github.com>
2024-11-24 05:01:24 +0530

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("changeText").addEventListener("click", () => {
-    const text = document.getElementById("displayText");
-    text.textContent = "You clicked the button!";
-});
