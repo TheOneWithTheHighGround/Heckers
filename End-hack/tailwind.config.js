eab84fea34b1acfbe68866a09625fb1d1a4ae910
Aadiarora151 <107919173+Aadiarora151@users.noreply.github.com>
2024-11-24 09:46:13 +0530

    Delete End-hack/tailwind.config.js

--- End-hack/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'], // Fixed the quotes and comma
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
